8611dd3badbfc738f5ed36bbd83dbf048b436965
Patrik <patrikdrean@gmail.com>
2020-02-05 18:01:35 -0700

    Reset values for testing demo

--- server/_config/config.js
+++ server/_config/config.js
@@ -1,23 +1,24 @@
+const dbName = 'todo'
+const testDbName = 'todo-test'
+
 export const configOptions = {
   development: {
-    db: 'mongodb://localhost/todo',
+    db: `mongodb://localhost/${dbName}`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   testDevelopment: {
-    db: 'mongodb://localhost/todo-integration',
+    db: `mongodb://localhost/${testDbName}`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   production: {
-    db:
-      'mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/production?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   testProduction: {
-    db:
-      'mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   }
