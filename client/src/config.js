4a93045e319649ac12a5cab04303afb50aae0629
Patrik <patrikdrean@gmail.com>
2020-02-07 07:13:43 -0700

    Fix apiUrl for client

--- client/src/config.js
+++ client/src/config.js
@@ -6,6 +6,6 @@ export const configOptions = {
     apiUrl: 'http://localhost:3001'
   },
   production: {
-    apiUrl: 'https://todo-patrik-test.herokuapp.com/'
+    apiUrl: 'https://todo-patrik-test.herokuapp.com'
   }
 }
