93d94cc02ef19f760c1fd62a77727294c8cebecc
Sarah Fankhauser <scfank99@byu.edu>
2020-02-12 19:20:14 -0700

    did class prep for 2/13

--- server/_config/config.js
+++ server/_config/config.js
@@ -13,12 +13,12 @@ export const configOptions = {
     userCookieName: 'user'
   },
   production: {
-    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb://sarahfank:is693rmongodb@cluster0-shard-00-00-q00nx.mongodb.net:27017,cluster0-shard-00-01-q00nx.mongodb.net:27017,cluster0-shard-00-02-q00nx.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   testProduction: {
-    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb://sarahfank:is693rmongodb@cluster0-shard-00-00-q00nx.mongodb.net:27017,cluster0-shard-00-01-q00nx.mongodb.net:27017,cluster0-shard-00-02-q00nx.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   }
